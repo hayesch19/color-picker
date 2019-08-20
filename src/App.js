d2dd0d3d4936d42670af44cda582ea0e72d26bf3
hayesch19 <chrishayespro@gmail.com>
2019-08-20 18:11:59 -0400

    V1 Color Sliders/Button Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import Header from './components/header-section'
+import ColorSelector from './components/color-selector'
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    console.log('COLOR!')
+    return (
+      <main>
+        <Header />
+        <ColorSelector />
+        <footer>
+          <h5>
+            Build a color slider in React they said, it will be fun they said
+          </h5>
+        </footer>
+      </main>
+    )
   }
 }
 
---
+++ src/components/color-selector.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react'
+
+class ColorSelector extends Component {
+  state = {
+    hue: Math.floor(Math.random() * 360),
+    saturation: Math.floor(Math.random() * 100),
+    lightness: Math.floor(Math.random() * 100),
+    hueInput: '',
+    saturationInput: '',
+    lightnessInput: ''
+  }
+  setHueInput = event => {
+    console.log(event.target.value)
+    this.setState({
+      hue: event.target.value
+      // hue: this.state.hueInput
+    })
+  }
+  setSaturationInput = event => {
+    console.log(event.target.value)
+    this.setState({
+      saturation: event.target.value
+      // saturation: this.state.saturationInput
+    })
+  }
+  setLightnessInput = event => {
+    console.log(event.target.value)
+    this.setState({
+      lightness: event.target.value
+      // lightness: this.state.lightnessInput
+    })
+  }
+
+  randomColor = event => {
+    this.setState({
+      hue: Math.floor(Math.random() * 360),
+      saturation: Math.floor(Math.random() * 100),
+      lightness: Math.floor(Math.random() * 100)
+    })
+  }
+
+  render() {
+    return (
+      <section>
+        <div className="results-area">
+          <p
+            className="color-results"
+            style={{
+              backgroundColor: `hsl(${this.state.hue},${
+                this.state.saturation
+              }% ,${this.state.lightness}%)`
+            }}
+          />
+        </div>
+        <div className="color-sliders">
+          <div className="unicorn">
+            <h3>Hue</h3>
+            <input
+              className="hue"
+              type="range"
+              min="1"
+              max="240"
+              value={this.state.hue}
+              onChange={this.setHueInput}
+            />
+          </div>
+          <div className="unicorn">
+            <h3>Saturation</h3>
+            <input
+              className="saturation"
+              type="range"
+              value={this.state.saturation}
+              onChange={this.setSaturationInput}
+            />
+          </div>
+          <div className="unicorn">
+            <h3>Lightness</h3>
+            <input
+              className="lightness"
+              type="range"
+              value={this.state.lightness}
+              onChange={this.setLightnessInput}
+            />
+          </div>
+          <h3 className="values">
+            {`hsl(${this.state.hue},${this.state.saturation}% ,${
+              this.state.lightness
+            }%)`}
+          </h3>
+          <button className="random-color-btn" onClick={this.randomColor}>
+            Get A Random Color
+          </button>
+        </div>
+      </section>
+    )
+  }
+}
+
+export default ColorSelector
